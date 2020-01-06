914e01f8f6847503aaea4d61cb1132bb32c6a971
Administrator <892895953@qq.com>
2020-01-06 09:28:21 +0800

    lx

--- README.md
+++
@@ -1,2 +0,0 @@
-"# test_selemium5-6" 
-"# test_selemium5-6" 
--- test_selenium/test_zuoye.py
+++ test_selenium/test_zuoye.py
@@ -2,7 +2,6 @@ import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pytest
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
